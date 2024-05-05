f8158b64bb3243c340c24db7001455a2345bf0ab
WeRockStar <kotchaphan.m@gmail.com>
2024-05-05 11:08:55 +0700

    add output db endpoint for public access

--- infra/iac/rds/main.tf
+++ infra/iac/rds/main.tf
@@ -1,25 +1,30 @@
 # Low cost instance
 resource "aws_db_instance" "postgres" {
-	allocated_storage            = 20
-	storage_type                 = "gp2"
-	engine                       = "postgres"
-	engine_version               = "16.1"
-	instance_class               = "db.t3.micro"
-	identifier                   = "database-workshop"
-	username                     = var.db_username
-	password                     = var.db_password
-	db_name                      = var.db_name
-	multi_az                     = false
-	skip_final_snapshot          = false
-	vpc_security_group_ids       = [aws_security_group.db-sg.id]
-	backup_retention_period      = 7
-	parameter_group_name         = "default.postgres16"
-	performance_insights_enabled = true
+	allocated_storage                     = 20
+	storage_type                          = "gp2"
+	engine                                = "postgres"
+	engine_version                        = "16.1"
+	instance_class                        = "db.t3.micro"
+	identifier                            = "database-workshop"
+	username                              = var.db_username
+	password                              = var.db_password
+	db_name                               = var.db_name
+	multi_az                              = false
+	skip_final_snapshot                   = false
+	vpc_security_group_ids                = [aws_security_group.db-sg.id]
+	backup_retention_period               = 7
+	parameter_group_name                  = "default.postgres16"
+	performance_insights_enabled          = true
 	performance_insights_retention_period = 7
+	publicly_accessible = true // DON'T DO THIS IN PRODUCTION
 
 	tags = {
 		Name = "database-workshop"
 	}
+}
+
+output "database_endpoint" {
+	value = aws_db_instance.postgres.endpoint
 }
 
 data "aws_vpc" "default" {
@@ -36,6 +41,13 @@ resource "aws_security_group" "db-sg" {
 		from_port   = 5432
 		to_port     = 5432
 		protocol    = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
