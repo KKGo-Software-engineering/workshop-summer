e4d5514fd5e3eb84c74c4da874892191ea03f951
WeRockStar <kotchaphan.m@gmail.com>
2024-05-08 18:42:35 +0700

    correct working directory for group pattern

--- auto-replace-group.sh
+++ auto-replace-group.sh
@@ -4,6 +4,6 @@ read -p "Enter group number (ex. group-1): " group_number
 
 group_pattern="<GROUP_NO>"
 
-working_directory="../"
+working_directory="./"
 
 find "$working_directory" -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "" "s/$group_pattern/$group_number/g" {} \;
