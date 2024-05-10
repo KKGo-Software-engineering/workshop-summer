95b0f8b5e7bdf27f8d9e0ac099379cd63b6131c4
WeRockStar <kotchaphan.m@gmail.com>
2024-05-10 16:40:05 +0700

    move fork and delete script into scripts directory

---
+++ scripts/auto-delete-repo.sh
@@ -0,0 +1,3 @@
+for i in {0..5}; do
+    gh repo delete https://github.com/KKGo-Software-engineering/workshop-summer-group-$i-b1
+done
---
+++ scripts/auto-fork.sh
@@ -0,0 +1,3 @@
+for i in {0..5}; do
+    gh repo fork --org KKGo-Software-engineering --fork-name workshop-summer-group-$i-b1
+done
