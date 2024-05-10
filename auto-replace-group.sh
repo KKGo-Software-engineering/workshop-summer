6df35c58b978be37356f0043ecb93ff27c0869e2
WeRockStar <kotchaphan.m@gmail.com>
2024-05-10 16:22:58 +0700

    add success message for auto replace group

--- auto-replace-group.sh
+++ auto-replace-group.sh
@@ -7,3 +7,5 @@ group_pattern="<GROUP_NO>"
 working_directory="./"
 
 find "$working_directory" -type f \( -name "*.yaml" -o -name "*.yml" \) -exec sed -i "" "s/$group_pattern/$group_number/g" {} \;
+
+echo "✅ DONE: $group_number is replaced in all yaml files."
