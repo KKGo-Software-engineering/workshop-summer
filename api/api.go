9e3626bfbf9eef3dd4f9614e1ed5034fbc594c58
Nong AnuchitO <anuchito.code@gmail.com>
2024-05-10 21:17:31 +0700

    move api new to package api

---
+++ api/api.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"database/sql"
+
+	"github.com/KKGo-Software-engineering/workshop-summer/api/config"
+	"github.com/KKGo-Software-engineering/workshop-summer/api/eslip"
+	"github.com/KKGo-Software-engineering/workshop-summer/api/health"
+	"github.com/KKGo-Software-engineering/workshop-summer/api/spender"
+	"github.com/labstack/echo/v4"
+)
+
+func New(db *sql.DB, cfg config.Config) *echo.Echo {
+	e := echo.New()
+
+	v1 := e.Group("/api/v1")
+
+	v1.GET("/slow", health.Slow)
+	v1.GET("/health", health.Check(db))
+	v1.POST("/upload", eslip.Upload)
+
+	{
+		h := spender.New(cfg.FeatureFlag, db)
+		v1.GET("/spenders", h.GetAll)
+		v1.POST("/spenders", h.Create)
+	}
+
+	return e
+}
--- main.go
+++ main.go
@@ -8,13 +8,10 @@ import (
 	"os/signal"
 	"time"
 
+	"github.com/KKGo-Software-engineering/workshop-summer/api"
 	"github.com/KKGo-Software-engineering/workshop-summer/api/config"
-	"github.com/KKGo-Software-engineering/workshop-summer/api/eslip"
-	"github.com/KKGo-Software-engineering/workshop-summer/api/health"
-	"github.com/KKGo-Software-engineering/workshop-summer/api/spender"
 	"github.com/KKGo-Software-engineering/workshop-summer/migration"
 	"github.com/kkgo-software-engineering/workshop/mlog"
-	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	_ "github.com/lib/pq"
@@ -38,7 +35,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	e := run(db, cfg)
+	e := api.New(db, cfg)
 
 	e.Use(middleware.Logger())
 	e.Use(mlog.Middleware(logger))
@@ -63,22 +60,4 @@ func main() {
 		logger.Fatal("shutting down the server:", zap.Error(err))
 	}
 	logger.Info("server shutdown gracefully")
-}
-
-func run(db *sql.DB, cfg config.Config) *echo.Echo {
-	e := echo.New()
-
-	v1 := e.Group("/api/v1")
-
-	v1.GET("/slow", health.Slow)
-	v1.GET("/health", health.Check(db))
-	v1.POST("/upload", eslip.Upload)
-
-	{
-		h := spender.New(cfg.FeatureFlag, db)
-		v1.GET("/spenders", h.GetAll)
-		v1.POST("/spenders", h.Create)
-	}
-
-	return e
 }
