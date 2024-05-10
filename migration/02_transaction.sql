30d33ba8da5e25923e150a77134085e4ea333c52
Nong AnuchitO <anuchito.code@gmail.com>
2024-05-10 21:14:18 +0700

    script create transaction table

---
+++ migration/02_transaction.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS "transaction" (
+  id SERIAL PRIMARY KEY,
+  date TIMESTAMP WITH TIME ZONE,
+  amount DECIMAL(10,2) DEFAULT 0,
+  category VARCHAR(50) DEFAULT '',
+  transaction_type VARCHAR(20) DEFAULT '',
+  note VARCHAR(255) DEFAULT '',
+  image_url VARCHAR(255) DEFAULT ''
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS "transaction";
+-- +goose StatementEnd
